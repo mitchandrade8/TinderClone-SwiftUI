1e4cbcb428ba15d753fbb8c3188b700a5c85ac83
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2021-11-26 23:57:32 -0700

    Initial Commit

---
+++ BuildTinderApp/BuildTinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTinderAppApp.swift
+//  BuildTinderApp
+//
+//  Created by Mitch Andrade on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct BuildTinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
